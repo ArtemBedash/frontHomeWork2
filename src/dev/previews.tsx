f6f1db1f030552511813b7ae7062aa6e19411221
Artem Bedash <bot300191@gmail.com>
2025-09-23 17:10:23 +0300

    HW5

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,9 +1,13 @@
-import {Previews} from "@react-buddy/ide-toolbox";
+import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
+import Pages from "../s2-homeworks/hw05/Pages";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Pages">
+                <Pages/>
+            </ComponentPreview>
         </Previews>
     );
 };
