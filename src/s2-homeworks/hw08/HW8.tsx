29d5849c4c20df32b6a5e7f378ff2c39dd6d8a22
Artem Bedash <bot300191@gmail.com>
2025-10-01 21:48:22 +0300

    HW8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -19,11 +19,11 @@ export type UserType = {
 
 const initialPeople: UserType[] = [
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот', age: 3},
+    {_id: 0, name: 'Кот', age: 17},
     {_id: 1, name: 'Александр', age: 66},
     {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 3, name: 'Виктор', age: 25},
+    {_id: 4, name: 'Дмитрий', age: 30},
     {_id: 5, name: 'Ирина', age: 55},
 ]
 
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,33 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+
+            if (action.payload === 'up') {
+
+                const sortedUp = [...state].sort((a, b) => a.name.localeCompare(b.name))
+
+                return sortedUp
+            }
+            if (action.payload === 'down') {
+
+                const sortedDown = [...state].sort((a, b) => b.name.localeCompare(a.name))
+
+                return sortedDown
+            }
 
             return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            const newState=  state.filter((user) => user.age >= action.payload )
+
+
+            return newState // need to fix
         }
         default:
             return state
